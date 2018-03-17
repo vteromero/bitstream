a8d67805d013964d9de1a0d4d8f9b2edcfcc5dc7
Vicente Romero <vteromero@gmail.com>
2018-03-17 15:09:54 +0000

    added MIT license

--- reader.go
+++ reader.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Vicente Romero. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE file in the project root for full license information.
+
 package bitstream
 
 // A Reader is a structure to read bits from a stream of bytes.
